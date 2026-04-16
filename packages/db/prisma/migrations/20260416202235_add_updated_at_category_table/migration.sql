6186a4a77a182a9c7e1d1e89402fbcd68e56b6c9
Marios <info@itdev.gr>
2026-04-16 23:22:43 +0300

    feat(db): add updatedAt to Category and Table

---
+++ packages/db/prisma/migrations/20260416202235_add_updated_at_category_table/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `Category` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Table` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Category" ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Table" ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
